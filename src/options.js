b81f43a9ed84e8c9b7a2d936ee8d3e84239f698e
Mercedes <mlapasset@udesa.edu.ar>
2024-09-15 19:26:06 -0300

    archivo options con la api key

---
+++ src/options.js
@@ -0,0 +1,7 @@
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNTU3MDkxMWVkMTM1Mjg0ZmQzOGRkZjgzZjJiMWQzZSIsIm5iZiI6MTcyNjQzNjE2NS44NTk2LCJzdWIiOiI2NmU0YWZlYzYwNzM1ZTk5MGQzYWNhZjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.tRkNAqC47opp3Tvb7gljQdezyJDZ6QWR2Ht2iouEDU8'
+    }
+  };
