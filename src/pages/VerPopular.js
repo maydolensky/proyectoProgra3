c2fc7778b1463264f073b6dd11b903c3a3d12d6f
Lilayasuff <lyasuff@udesa.edu.ar>
2024-09-23 18:12:10 -0300

    ver mas y filtrado de populares. Falta agregar el Loader

--- src/pages/VerPopular.js
+++ src/pages/VerPopular.js
@@ -7,7 +7,9 @@ export default class VerPopular extends Component {
     super(props)
     this.state = {
       value: 1,
-      peliculas: []
+      peliculas: [],
+      PelisFiltradas: [],
+      filterValue: ""
     }
   }
   handleIncrement() {
@@ -15,25 +17,38 @@ export default class VerPopular extends Component {
     fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${mas}`, options)
       .then(response => response.json())
       .then(data => this.setState(pelisAntes => ({
-        peliculas: [...pelisAntes.peliculas, ...data.results],
+        peliculas: this.state.peliculas.concat(data.results),
+        PelisFiltradas: this.state.peliculas.concat(data.results),
         value: mas
       })))
       .catch(error => console.log(error));
   };
-
   componentDidMount() {
     fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${this.state.value}`, options)
       .then(response => response.json())
-      .then(data => this.setState({ peliculas: data.results }))
-
+      .then(data => this.setState({ peliculas: data.results, PelisFiltradas: data.results}))
       .catch(error => console.log(error));
   }
-
-
+handleFilterChange(e){
+  const userValue = e.target.value
+  this.setState({
+    filterValue: userValue,
+    PelisFiltradas: this.state.peliculas.filter(peliculas => peliculas.title.toLowerCase().includes(userValue.toLowerCase()))
+  })
+}
+handleResetFilter(){
+  this.setState({
+    filterValue: "",
+    PelisFiltradas: this.state.peliculas
+  })
+}
   render() {
     return (
       <>
-        <PeliculaGrid peliculas={this.state.peliculas} />
+        <h1>Peliculas Populares</h1>
+        <input type="text" onChange={(e)=> this.handleFilterChange(e)} value= {this.state.filterValue}/>
+        <button onClick={()=> this.handleResetFilter()}>Reset Filter</button>
+        <PeliculaGrid peliculas={this.state.PelisFiltradas} />
         <button onClick={() => this.handleIncrement()}>Ver mas</button>
       </>
     )
