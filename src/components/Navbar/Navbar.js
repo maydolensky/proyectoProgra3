209397fc2b155135df62e6a0e06a253b61ef6a66
Lilayasuff <lyasuff@udesa.edu.ar>
2024-09-27 10:41:05 -0300

    estilos y responsive del header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import "./Navbar.css"
 
-function Navbar(props) {
-
+class Navbar extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            desplegable: false,
+        }
+    
+    }
+handleDesplegable = () =>{
+    this.setState({
+        desplegable: !this.state.desplegable
+    })
+}
+render (){
     return (
         <nav>
             <ul className="user">
                 <li> <img src="/img/pelis+.png" alt="" /></li>
             </ul>
-            <ul className="main-nav">
-                {props.elementosMenu.map((unMenu, idx) => (
+            <div className = {`menu-icon ${this.state.desplegable ? "active": ""}`}onClick={this.handleDesplegable}>
+                <i className='fas fa-bars'></i>
+            </div>
+            <ul className= {`main-nav ${this.state.desplegable ? "active": ""}`}>
+                {this.props.elementosMenu.map((unMenu, idx) => (
                     <li key={idx}>
                         {unMenu.link ? (
                             <Link to={unMenu.link}>{unMenu.name}</Link>
@@ -25,5 +40,5 @@ function Navbar(props) {
         </nav >
     )
 }
-
+}
 export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
